37d501dbd2c1652e69d47c17d31337e264b2569f
skg1998 <sahilkg1998@gmail.com>
2020-08-19 09:05:32 +0530

    work on user-table

--- dashboard/src/app/table-list/table-list.component.ts
+++ dashboard/src/app/table-list/table-list.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableListComponent implements OnInit {
 
+  userData:any = [
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+    {id: "1", Name: "sahil gupta", country: "india", Date:"19-08-2020", entryTime: "08:35",exitTime:'08:35',totalDuration:'20 min 10 sec'},
+  ];
+  _object = Object;
+
+
   constructor() { }
 
   ngOnInit() {
