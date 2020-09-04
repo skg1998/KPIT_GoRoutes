216b18d52ecd75e8bf55334f82e356e31d5bd30d
skg1998 <sahilkg1998@gmail.com>
2020-09-04 10:48:05 +0530

    work on map-path-detection

--- dashboard/src/app/maps/maps.component.ts
+++ dashboard/src/app/maps/maps.component.ts
@@ -19,8 +19,8 @@ export class MapsComponent implements OnInit {
 
   map: mapboxgl.Map;
   style = 'mapbox://styles/codingboy/ckdr6586v0o0419oolwcu3qg8'; //mapbox://styles/codingboy/ckdr6586v0o0419oolwcu3qg8
-  lat = 26.5775;
-  lng = 93.1711;
+  lat = -122.486052 //37.830348//26.5775;
+  lng = 37.830348//93.1711;
 
   constructor() { }
   ngOnInit() {
@@ -29,11 +29,48 @@ export class MapsComponent implements OnInit {
         container: 'map',
         style: this.style,
         zoom: 13,
-        center: [this.lng, this.lat]
+        center:  [35.1715959,31.9305916,],// [this.lng, this.lat]
     });
     // Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());
 
+    this.map.on('load', () => {
+      this.map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+        'type': 'LineString',
+        'coordinates': [
+          [35.20620346069335,31.941237613275092],
+          [35.21341323852539,31.952162238024975],
+          [35.21392822265624,31.961337918930735],
+          [35.21100997924805,31.965998231173806],
+          [35.19899368286133,31.971968910549975],
+          [35.187835693359375,31.97240577428203],
+          [35.17822265625,31.967163272276267],
+          [35.173072814941406,31.958133817062862],
+          [35.17066955566406,31.950560041013226]
+          ]
+         }
+        }
+      });
+
+      this.map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': 'red',
+        'line-width': 8
+        }
+        });
+    })
 
     //// Add Marker on Click
     this.map.on('click', (event) => {
